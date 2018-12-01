96688a332eee96097bacc5468afb70d2adc0f4ce
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-12-01 15:03:27 +0200

    BookDaoPG refaktorointia

--- src/main/java/ohtu/dao/BookDaoPG.java
+++ src/main/java/ohtu/dao/BookDaoPG.java
@@ -1,6 +1,5 @@
 package ohtu.dao;
 
-import java.sql.Timestamp;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,80 +12,125 @@ import ohtu.model.Book;
 import org.h2.tools.RunScript;
 
 /**
- * BookDao implementation for postgresql database.
+ * BookDao implementation for postgreSQL database.
  */
 public class BookDaoPG implements BookDao {
-
+    
+    private final int PRDSTM_INDEX_1 = 1;   // constant for preparedstatement variable placement
+    private final int PRDSTM_INDEX_2 = 2;   // constant for preparedstatement variable placement
+    private final int PRDSTM_INDEX_3 = 3;   // constant for preparedstatement variable placement
+    private final int PRDSTM_INDEX_4 = 4;   // constant for preparedstatement variable placement
+    private final int PRDSTM_INDEX_5 = 5;   // constant for preparedstatement variable placement
+    
+    /**
+     * Add book to database.
+     *
+     * @param book
+     */
     @Override
     public void add(Book book) {
-        try {
-            // get connection
-            Connection connection = getDatabaseConnection();
-            if (connection == null) {
-                return;
+        // get database connection
+        Connection connection = getDatabaseConnection();
+        // proceed if connection is not null
+        if (connection != null) {
+            // try to add book to database
+            try {
+                addBookToDatabase(connection, book);
+            } catch (Exception e) {
+                // operation failed... print error message
+                e.printStackTrace();
             }
-            addBookToDatabase(connection, book);
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+            // connection has not been closed yet, so close it now
+            closeDatabaseConnection(connection);
         }
     }
 
+    /**
+     * Get list of all books in database.
+     *
+     * @return list of books
+     */
     @Override
     public List<Book> list() {
-        // get connection to database
+        List<Book> books = new ArrayList<>();
+        // get databse connection
         Connection connection = getDatabaseConnection();
-        if (connection == null) {
-            return new ArrayList<>();
-        }
-        // get list of books
-        List<Book> books = fetchAllBooks(connection);
-        try {
-            connection.close();
-        } catch (Exception e) {
+        // proceed if connection is not null
+        if (connection != null) {
+            // try to get all books from database
+            try {
+                books = fetchAllBooks(connection);
+            } catch (Exception e) {
+                // operation failed... print error message
+                e.printStackTrace();
+            }
+            // connection has nod been closed yet, so close it now
+            closeDatabaseConnection(connection);
         }
         return books;
     }
 
+    /**
+     * Get book from database with specific id.
+     *
+     * @param id book id
+     * @return book
+     */
     @Override
     public Book getBook(int id) {
+        Book book = null;
+        // get database connection
         Connection connection = getDatabaseConnection();
-        if (connection == null) {
-            return null;
-        }
-        Book book = getBookById(connection, id);
-        try {
-            connection.close();
-        } catch (Exception e) {
+        // proceed if connection is not null
+        if (connection != null) {
+            // try to retrieve book from database
+            try {
+                book = getBookById(connection, id);
+            } catch (Exception e) {
+                // operation failed... print error message
+            }
+            // connection has not been closed yet, so close it now
+            closeDatabaseConnection(connection);
         }
         return book;
     }
 
+    /**
+     * Update book in database.
+     *
+     * @param book
+     */
     @Override
     public void update(Book book) {
-        try {
-            Connection connection = getDatabaseConnection();
-            if (connection == null) {
-                return;
+        // get database connection
+        Connection connection = getDatabaseConnection();
+        // proceed if connection is not null
+        if (connection != null) {
+            // try to update the book in database
+            try {
+                updateBook(connection, book);
+            } catch (Exception e) {
+                // operation failed... print error message
+                e.printStackTrace();
             }
-            this.updateBook(connection, book);
-        } catch (Exception e) {
-            e.printStackTrace();
+            // connection has no been closed yet, sol close it now
+            closeDatabaseConnection(connection);
         }
     }
 
+    /**
+     * Helper method for getting database connection.
+     */
     private Connection getDatabaseConnection() {
         try {
             // Open connection to a database -- do not alter this code
-
             //Try to get db address from env. variable if possible
             String databaseAddress = System.getenv("JDBC_DATABASE_URL");
             if (databaseAddress == null || databaseAddress.length() == 0) {
                 databaseAddress = "jdbc:postgresql:" + System.getenv("DB_NAME");
             }
             Connection connection = DriverManager.getConnection(databaseAddress, System.getenv("DB_USER"), System.getenv("DB_PASSWORD"));
-            try {
-                // If database has not yet been created, insert content --> maybe this should be done somewhere else
+            try { // If database has not yet been created, insert content
                 RunScript.execute(connection, new FileReader("sql/database-schema.sql"));
                 RunScript.execute(connection, new FileReader("sql/database-import.sql"));
             } catch (Throwable t) {
@@ -98,83 +142,99 @@ public class BookDaoPG implements BookDao {
         return null;
     }
 
-    private List<Book> fetchAllBooks(Connection connection) {
+    /**
+     * Helper method for closing database connection.
+     *
+     * @param connection
+     */
+    private void closeDatabaseConnection(Connection connection) {
         try {
-            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM BOOK");
-            List<Book> books = new ArrayList<>();
-
-            while (resultSet.next()) {
-                Book book = createBookFromResultSetRow(resultSet);
-                if (book != null) {
-                    books.add(book);
-                }
-            }
-
-            resultSet.close();  // close resultset
-            return books;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ArrayList<>();
-    }
-
-    private Book createBookFromResultSetRow(ResultSet resultSet) {
-        try {
-            int id = resultSet.getInt("ID");
-            String writer = resultSet.getString("WRITER");
-            String title = resultSet.getString("TITLE");
-            String isbn = resultSet.getString("ISBN");
-            String description = resultSet.getString("DESCRIPTION");
-            boolean isRead = resultSet.getBoolean("IS_READ");
-            Date date = null;
-            if (isRead) {
-                date = new Date(resultSet.getTimestamp("DATE_OF_READ").getTime());
-            }
-            Book book = new Book(id, title, writer, isbn, description, isRead, date);
-            return book;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void addBookToDatabase(Connection connection, Book book) {
-
-        try {
-            String statement = "INSERT INTO BOOK (WRITER, TITLE, ISBN, DESCRIPTION) VALUES ('"
-                    + book.getWriter() + "', '" + book.getTitle() + "', '" + book.getIsbn() + "', '" + book.getDescription() + "')";
-
-            connection.createStatement().executeUpdate(statement);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Book getBookById(Connection connection, Integer id) {
-        try {
-            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM BOOK WHERE ID='" + id + "'");
-            Book book = null;
-
-            if (resultSet.next()) {
-                book = createBookFromResultSetRow(resultSet);
-            }
-            resultSet.close();
-            return book;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void updateBook(Connection connection, Book book) {
-        try {
-            PreparedStatement prdstm = connection.prepareStatement("UPDATE BOOK SET "
-                    + "WRITER = '" + book.getWriter() + "', TITLE = '" + book.getTitle() + "', ISBN = '" + book.getIsbn() + "', DESCRIPTION = '" + book.getDescription() + "' WHERE ID = '" + book.getId() + "';");
-            prdstm.execute();
-
+            // try to close the databse connection
             connection.close();
         } catch (Exception e) {
+            // something went wrong and connection was not closed properly
+            // print the error message
             e.printStackTrace();
         }
     }
+
+    /**
+     * Helper method for getting all books in database.
+     */
+    private List<Book> fetchAllBooks(Connection connection) throws Exception {
+        String query = "SELECT * FROM BOOK;";
+        PreparedStatement prdstm = connection.prepareStatement(query);
+        ResultSet resultSet = prdstm.executeQuery();
+        List<Book> books = new ArrayList<>();
+        while (resultSet.next()) {
+            Book book = createBookFromResultSet(resultSet);
+            if (book != null) {
+                books.add(book);
+            }
+        }
+        resultSet.close();
+        return books;
+    }
+
+    /**
+     * Helper method for creating book model from resultSet.
+     */
+    private Book createBookFromResultSet(ResultSet resultSet) throws Exception {
+        int id = resultSet.getInt("ID");
+        String writer = resultSet.getString("WRITER");
+        String title = resultSet.getString("TITLE");
+        String isbn = resultSet.getString("ISBN");
+        String description = resultSet.getString("DESCRIPTION");
+        boolean isRead = resultSet.getBoolean("IS_READ");
+        Date date = null;
+        if (isRead) {
+            date = new Date(resultSet.getTimestamp("DATE_OF_READ").getTime());
+        }
+        Book book = new Book(id, title, writer, isbn, description, isRead, date);
+        return book;
+    }
+
+    /**
+     * Helper method for adding book to database.
+     */
+    private void addBookToDatabase(Connection connection, Book book) throws Exception {
+        String statement = "INSERT INTO BOOK (WRITER, TITLE, ISBN, DESCRIPTION) VALUES (?, ?, ?, ?);";
+        PreparedStatement prdstm = connection.prepareStatement(statement);
+        prdstm.setString(PRDSTM_INDEX_1, book.getWriter());
+        prdstm.setString(PRDSTM_INDEX_2, book.getTitle());
+        prdstm.setString(PRDSTM_INDEX_3, book.getISBN());
+        prdstm.setString(PRDSTM_INDEX_4, book.getDescription());
+        prdstm.execute();
+    }
+
+    /**
+     * Helper method for getting book from database with specific id.
+     */
+    private Book getBookById(Connection connection, Integer id) throws Exception {
+        String query = "SELECT * FROM BOOK WHERE ID='?';";
+        PreparedStatement prdstm = connection.prepareStatement(query);
+        prdstm.setInt(PRDSTM_INDEX_1, id);
+        ResultSet resultSet = prdstm.executeQuery();
+        Book book = null;
+        if (resultSet.next()) {
+            book = createBookFromResultSet(resultSet);
+        }
+        resultSet.close();
+        return book;
+    }
+
+    /**
+     * Helper method for updating book in database.
+     */
+    private void updateBook(Connection connection, Book book) throws Exception {
+        String statement = "UPDATE BOOK SET WRITER = '?', TITLE = '?', ISBN = '?', DESCRIPTION = '?' WHERE ID = '?';";
+        PreparedStatement prdstm = connection.prepareStatement(statement);
+        prdstm.setString(PRDSTM_INDEX_1, book.getWriter());
+        prdstm.setString(PRDSTM_INDEX_2, book.getTitle());
+        prdstm.setString(PRDSTM_INDEX_3, book.getISBN());
+        prdstm.setString(PRDSTM_INDEX_4, book.getDescription());
+        prdstm.setInt(PRDSTM_INDEX_5, book.getId());
+        prdstm.execute();
+    }
+
 }
--- src/test/java/ohtu/dao/BookDaoPGTest.java
+++ src/test/java/ohtu/dao/BookDaoPGTest.java
@@ -1,55 +1,38 @@
-
 package ohtu.dao;
 
-
 import ohtu.model.Book;
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Assert;
 import org.junit.Before;
-//import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-//import java.sql.Timestamp;
-//import java.io.FileReader;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-//import org.h2.tools.RunScript;
 
-/**
- *
- * @author root
- */
 public class BookDaoPGTest {
-    
+
     BookDaoPG bdaopg;
     private Book book;
     private Date today;
     private Date changedDate;
-    
+
     @Before
     public void setUp() {
-        bdaopg = new BookDaoPG(); 
+        bdaopg = new BookDaoPG();
     }
 
     @Test
-    public void listBooksNotNull(){
-       List<Book> books = bdaopg.list();
-       assertNotEquals(null, books);
+    public void listBooksNotNull() {
+        List<Book> books = bdaopg.list();
+        assertNotEquals(null, books);
     }
-    
+
     @Test
-    public void titleOfFirstBookDatabaseImportSql(){
+    public void titleOfFirstBookDatabaseImportSql() {
         List<Book> books = bdaopg.list();
         assertEquals("Clean Code: A Handbook of Agile Software Craftsmanship", books.get(0).getTitle());
     }
-    
+
     @Test
-    public void addNewBook(){
+    public void addNewBook() {
         int id = 3;
         String writer = "HaamuKirjoittaja";
         String title = "Tietokannan salat";
@@ -61,7 +44,7 @@ public class BookDaoPGTest {
         book = new Book(id, title, writer, isbn, description, isRead, today);
         bdaopg.add(book);
         List<Book> books = bdaopg.list();
-        assertEquals("Tietokannan salat", books.get(books.size()-1).getTitle());
-   }
-    
+        assertEquals("Tietokannan salat", books.get(books.size() - 1).getTitle());
+    }
+
 }
