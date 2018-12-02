ef74e418ff4c41e5c6aa06d3bb678056d1c08433
Topi62 <toivo.karkinen@helsinki.fi>
2018-12-02 18:23:55 +0200

    allDao teststubs

---
+++ src/main/java/ohtu/teststubs/AllDaoForTests.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.teststubs;
+
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.dao.AllDao;
+import ohtu.model.Tip;
+
+/**
+ *
+ * @author tkarkine
+ */
+public class AllDaoForTests implements AllDao {
+    List<Tip> tips;
+    
+    @Override
+    public List<Tip> list() {
+       tips = new ArrayList<>();
+       return tips;
+    }
+    
+}
